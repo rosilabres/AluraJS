43fdbfaba8b6d3fce63540e48f93c89e6a0880d2
rosilabres <rosi.labres@gmail.com>
2017-05-06 20:46:53 -0300

    aula 4

--- js/principal.js
+++ js/principal.js
@@ -1,18 +1,39 @@
-var titulo =  document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
+	var titulo =  document.querySelector(".titulo");
+	titulo.textContent = "Aparecida Nutricionista";
 
-var pacientes = document.querySelectorAll(".paciente");
+	var pacientes = document.querySelectorAll(".paciente");
 
-for (var i = 0; i < pacientes.length; i++){
-  var tdPeso = pacientes[i].querySelector(".info-peso");
-  var peso = tdPeso.textContent;
+	for (var i = 0; i < pacientes.length; i++){
+	  var tdPeso = pacientes[i].querySelector(".info-peso");
+	  var peso = tdPeso.textContent;
 
-  var tdAltura = pacientes[i].querySelector(".info-altura");
-  var altura = tdAltura.textContent;
+	  var tdAltura = pacientes[i].querySelector(".info-altura");
+	  var altura = tdAltura.textContent;
 
-  var tdImc = pacientes[i].querySelector(".info-imc");
+	  var tdImc = pacientes[i].querySelector(".info-imc");
 
-  var imc = peso / (altura * altura);
+	  
+	  var pesoEhValido = true;
+	  var alturaEhValida = true;
 
-  tdImc.textContent = imc.toFixed(2);
-}
+	  if (peso <= 0 || peso >= 1000){
+	  	tdImc.textContent = "Peso inválido!";
+	  	pesoEhValido = false;
+	  	pacientes[i].classList.add("paciente-invalido");
+	  }
+
+	  if (altura <= 0 || altura >=3.00) {
+	  	tdImc.textContent = "Altura inválida!";
+	  	alturaEhValida = false;
+	  	 pacientes[i].classList.add("paciente-invalido");
+
+	  }
+
+	  if (pesoEhValido && alturaEhValida){
+	  	var imc = peso / (altura * altura);
+	  	tdImc.textContent = imc.toFixed(2);
+	  }
+	}
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", );
